0e484075551ba890af40b7cef709200dde34ba41
MIMFaisal <muinfaisal@gmail.com>
2024-03-02 15:55:45 +0600

    Updated File structure and build

--- config/esbuildMinified.js
+++ config/esbuildMinified.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { build } from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 import postcss from 'postcss';
@@ -9,7 +10,7 @@ const buildPath = `${sourcePath}/build`;
 
 build({
   entryPoints: [`${sourcePath}/index.js`, `${sourcePath}/scss/index.scss`],
-  entryNames: `${SITE}-${ID}_${VAR}.min`,
+  entryNames: `${ID}_${VAR}.min`,
   bundle: true,
   outdir: `${buildPath}`,
   minify: true,
@@ -17,7 +18,7 @@ build({
   plugins: [
     sassPlugin({
       async transform(source) {
-        const { css } = await postcss([autoprefixer]).process(source, { from: undefined });
+        const { css } = await postcss([autoprefixer({ flexbox: 'no-2009', grid: 'no-autoplace' })]).process(source, { from: undefined });
         return css;
       }
     })
@@ -25,4 +26,17 @@ build({
   treeShaking: true,
   charset: 'utf8',
   target: ['es2016']
+}).then(() => {
+  const css = fs.readFileSync(`${buildPath}/${ID}_${VAR}.min.css`, 'utf8');
+
+  const cssTemplate = `(()=>{const s=document.createElement('style');s.classList.add('${ID}_${VAR}-css');s.innerHTML=\`${css.trim()}\`;(function p(){if(document.head){document.head.appendChild(s);}else{setTimeout(p,25);}}())})();\n`;
+  fs.writeFileSync(`${buildPath}/${ID}_${VAR}.bundle.min.js`, cssTemplate, (err) => {
+    if (err) throw err;
+  });
+
+  const buildjs = fs.readFileSync(`${buildPath}/${ID}_${VAR}.min.js`, 'utf8');
+
+  fs.appendFile(`${buildPath}/${ID}_${VAR}.bundle.min.js`, buildjs, (err) => {
+    if (err) throw err;
+  });
 });
